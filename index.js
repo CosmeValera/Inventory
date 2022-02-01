8fbc265c45d765e38975332c136629bf8e7acc55
CosmeValera <cosmevalerareales@gmail.com>
2022-02-01 01:20:06 +0100

    improve pug tepmlate and write down an example

---
+++ index.js
@@ -0,0 +1,8 @@
+//Importamos los instrumentos que me he dejado en el instrumentsToImport.json
+//los metemos en mongodb usando mongoose. (1° eliminamos los que hubiera)
+//A partir de ahora cada vez que vayamos a anadird un instrumento lo
+//insertamos a mongo. Cuando filtremos, filtramos en base a los que hay en mongo,
+//como primer approach en vez de crear otra pagina para añadir instrumento,
+//darle y que añada uno a piñon como hice en chirihomeweb
+//En un futuro tendre que implementar un servidor con express que escuche al
+//cliente parar madnarlo a otras pagainas tal vez (add instrumento, ver registro...)
