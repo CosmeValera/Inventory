7006cf58d22c9a780a2e7323eaf0c0c5e4b44df7
CosmeValera <cosmevalerareales@gmail.com>
2022-02-05 13:40:35 +0100

    add zoom and different sizzes for images

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,35 @@
-async function loadInstrumentsFromDB() {
+async function loadInstrumentsFromDB(evt) {
+  console.log(evt);
+  // console.log(evt.target);
+  // console.log(evt.target.value);
   const response = await fetch("/inventory");
 
   if (response.ok) {
     var instrumentsJson = await response.json();
-    document.querySelector("tbody").innerHTML = insertInstruments({
-      instruments: instrumentsJson,
-    });
+    if (evt) {
+      //When clicking the checkbox
+      if (evt.target.checked) {
+        document.querySelector("tbody").innerHTML = insertBigInstruments({
+          instruments: instrumentsJson,
+        });
+      } else {
+        document.querySelector("tbody").innerHTML = insertInstruments({
+          instruments: instrumentsJson,
+        });
+      }
+    } else {
+      //Load screen first time
+      document.querySelector("tbody").innerHTML = insertInstruments({
+        instruments: instrumentsJson,
+      });
+    }
   } else {
     alert("Server found an issue, " + response.statusText);
   }
 }
+
+document
+  .getElementById("switchBigImg")
+  .addEventListener("change", loadInstrumentsFromDB);
 
 loadInstrumentsFromDB();
