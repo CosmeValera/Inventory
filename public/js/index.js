e489ad1ebd95690aaff5b747cb574f354d5a4d49
CosmeValera <cosmevalerareales@gmail.com>
2022-02-07 02:00:12 +0100

    put modal page in pug instead of innerHtml in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -44,35 +44,12 @@ async function linkOrImageClicked(evt) {
 
     //Modal inits
     modalContent = modal.querySelector(".modal-content");
-    modalContent.innerHTML = `
-    <div class="modal-header" id= "container-fill-height">
-      <h2 class="my-0"><strong>${instrumentJson.name}</strong></h2>
-      <span class="close item-upper-right px-4 py-3">&times;</span>
-    </div>
-    <div class = "modal-body">
-      <p class="d-flex justify-content-center my-0"><strong class="me-2">Type:</strong> ${instrumentJson.type}</p>`
-      +((instrumentJson.subtype)
-        ?`<p class="d-flex justify-content-center my-0"><strong class="me-2">Subtype:</strong> ${instrumentJson.subtype} </p>`
-        :``)
-      +`<p class="d-flex justify-content-center my-0"><strong class="me-2">Price:</strong> $${instrumentJson.price} </p>
-      <p class="d-flex justify-content-center my-0"><strong class="me-2">Sonority:</strong> ${instrumentJson.sonority} </p>`
-      +((instrumentJson.summary)
-        ?`<p class="d-flex justify-content-center mt-0 mx-5"><strong class="me-2">Summary:</strong> ${instrumentJson.summary}</p>`
-        :`<p class="d-flex justify-content-center mt-0 mb-3"><strong class="me-2">No Summary</strong></p>`)
-      +`<a class="d-flex justify-content-center" href="#">
-        <img
-          class="img-fluid"
-          src="img/`
-          +((instrumentJson.name!="Snare Drum")
-          ?`${instrumentJson.name}`
-          :`snareDrum`)
-          +`.png"
-          alt="img"/>
-      </a>
-    </div>`;
-    document.querySelector(".close") .onclick = function() {
+    modalContent.innerHTML = insertModalInstrument({
+      instrument: instrumentJson,
+    });
+    document.querySelector(".close").onclick = function () {
       modal.style.display = "none";
-    }
+    };
     modal.style.display = "block";
   } else {
     alert("Server found an issue, " + response.statusText);
@@ -89,9 +66,9 @@ document.querySelector("tbody").addEventListener("click", linkOrImageClicked);
 
 //Modal inits
 modal = document.getElementById("myModal");
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 //Modal inits
