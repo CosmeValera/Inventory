4670361d5a43b796d1b3b871212995163529d5c7
CosmeValera <cosmevalerareales@gmail.com>
2022-02-10 17:43:30 +0100

    register similar css to index

--- public/js/register.js
+++ public/js/register.js
@@ -1,24 +1,13 @@
 async function loadRecordsFromDBToTable() {
     const response = await fetch("/register");
     if (response.ok) {
-        // var recordsJson = await response.json();
-        var recordsJson = {
-            _id: "123",
-            type: "add",
-            summary: "saxophone was added",
-        };
-        console.log(recordsJson);
+        var recordsJson = await response.json();
         var dataPug = {
             records: recordsJson,
             // bigInstruments: settings.bigInstruments,
         };
 
-        console.log(document.querySelector("tbody"));
-        // console.log(
-        //     insertRecords({
-        //         records: recordsJson,
-        //     })
-        // );
+        console.log(dataPug);
         document.querySelector("tbody").innerHTML = insertRecords({
             data: dataPug,
         });
